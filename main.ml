f77b21b9efdc982bd9f58deaa0cea5d4472633e3
gduboc <guillaume.duboc@ens-lyon.fr>
2018-03-13 19:25:50 +0100

    Added repl

--- main.ml
+++ main.ml
@@ -1,18 +1,32 @@
 open Csv;;
 open Ast;;
 open Interpret;;
+open Pervasives;;
 
+type infos = {mutable file : string; mutable data : data option; mutable expr : operator option};;
 
-let test () =
+let create_info () =
+	{file = ""; data = None; expr = None}
+
+(* let test () =
   let ui = read_csv ("cars.csv") in
   let op = Projection (Relation ui, ["Year"; "Model"]) in 
   let c = eval op in 
   print c ;;
 
-test ();;
+test ();; *)
 
 
+(* write_to_csv (read_csv ("cars.csv")) "test.csv";; *)
 
+let rec repl () = 
+	let infos = create_info () in
+	let _ = print_endline " --- Welcome to simeon's miniSQL engine repl ! --- " in
+	let _ = print_string "|> " in
+		let lexbuf = read_line () in 
+		let _ = print_string lexbuf in
+		let _ = print_endline "" in 
+	repl ()
+;; 
 
-
-write_to_csv (read_csv ("cars.csv")) "test.csv";;
+repl ()
