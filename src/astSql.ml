29e6c45d4c5197040f1f4ef091605ccb95546142
gduboc <guillaume.duboc@ens-lyon.fr>
2018-03-14 02:04:25 +0100

    Beautified sql ast

--- src/astSql.ml
+++ src/astSql.ml
@@ -3,30 +3,29 @@ type s = Select of (atts * rels * cond)
        | Minus of (s * s)
        | Union of (s * s)
 and
- atts = CoupleAtt of attd * atts 
-      | Single of attd
+     atts = CoupleAtt of attd * atts 
+          | Single of attd
 and
- attd = Lname of att
-      | Sname of (att * string)
+     attd = Lname of att
+          | Sname of (att * string)
 and
- att = string * string
-
+     att = string * string
 and
- rels = CoupleRel of rel * rels 
-      | SingleRel of rel
+     rels = CoupleRel of rel * rels 
+          | SingleRel of rel
 and
- rel = Filename of string
-     | Newtable of s * string
+     rel = Filename of string
+         | Newtable of s * string
 and
- cond = OrCons of and_cond * cond 
-      | SingleAnd of and_cond
+     cond = OrCons of and_cond * cond 
+          | SingleAnd of and_cond
 and
- and_cond = AndCond of at_cond * and_cond
-          | SingleAt of at_cond
+     and_cond = AndCond of at_cond * and_cond
+              | SingleAt of at_cond
 and
- at_cond = Eq of att * att
-         | Lt of att * att
-         | In of att * s
-         | NotIn of att * s
+     at_cond = Eq of att * att
+             | Lt of att * att
+             | In of att * s
+             | NotIn of att * s
 
 
