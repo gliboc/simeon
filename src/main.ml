6787060b0746f54e637ede42769620d35f58782b
Lapinot <peio.borthelle@ens-lyon.fr>
2018-03-26 16:53:51 +0200

    Renaming and formatting

--- src/main.ml
+++ src/main.ml
@@ -6,20 +6,20 @@ open AstAlg
 let rec repl () =
   let _ = print_string "|> " in
   let stream = Lexing.from_string (read_line ()) in
-  let _ =  
+  let _ =
     let query = Parser.main Lexer.token stream in
     begin
-    	Printf.printf "Your query: %s\n" (AstSql.show_query query);
-    	let bytecode = Compiler.compile query in 
-     		let _ = Printf.printf "Expression: %s\n" (show_operator bytecode)
-     		in Data.read_data (Interpreter.eval bytecode).inst
+      Printf.printf "Your query: %s\n" (AstSql.show_query query);
+      let bytecode = Compiler.compile query in
+      let _ = Printf.printf "Expression: %s\n" (show_operator bytecode)
+      in Data.read_data (Interpreter.eval bytecode).inst
     end
   in repl ()
 
 let wrap_repl () =
-  let _ = print_endline 
-"  _____ ____  ___ ___    ___   ___   ____  
- / ___/|    ||   |   |  /  _] /   \\ |    \\ 
+  let _ = print_endline
+"  _____ ____  ___ ___    ___   ___   ____
+ / ___/|    ||   |   |  /  _] /   \\ |    \\
 (   \\_  |  | | _   _ | /  [_ |     ||  _  |
  \\__  | |  | |  \\_/  ||    _]|  O  ||  |  |
  /  \\ | |  | |   |   ||   [_ |     ||  |  |
@@ -30,24 +30,15 @@ let wrap_repl () =
 
 let () =
   if Array.length Sys.argv > 1 then
-    let fi = Sys.argv.(1) in
-	let chan = open_in fi in
-    	let lines = ref "" in
-    	try
-           while true; do
-              lines := !lines ^ "\n" ^ input_line chan
-           done; 
-        with End_of_file ->
-	   close_in chan;
-           let stream = Lexing.from_string (!lines) in
- 	   let query = Parser.main Lexer.token stream in
+    let chan = open_in Sys.argv.(1) in
+    let stream = Lexing.from_channel chan in
+    let query = Parser.main Lexer.token stream in
     begin
-    	Printf.printf "Your query: %s\n" (AstSql.show_query query);
-    	let bytecode = Compiler.compile query in 
-     		let _ = Printf.printf "Expression: %s\n" (show_operator bytecode)
-     		in Data.read_data (Interpreter.eval bytecode).inst
+      close_in chan;
+      Printf.printf "Your query: %s\n" (AstSql.show_query query);
+      let bytecode = Compiler.compile query in
+      let _ = Printf.printf "Expression: %s\n" (show_operator bytecode) in
+      Data.read_data (Interpreter.eval bytecode).inst;
     end
   else
-    wrap_repl ()                       
-
-
+    wrap_repl ()
